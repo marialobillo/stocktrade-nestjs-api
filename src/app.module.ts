10e4f83b8130ce2bb4f8d72c12a06a0a64fc0081
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-10-28 18:05:06 +0200

    modules for companies and holdings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { CompaniesModule } from './companies/companies.module';
+import { HoldingsModule } from './holdings/holdings.module';
 
 @Module({
-  imports: [],
+  imports: [CompaniesModule, HoldingsModule],
 })
 export class AppModule {}
