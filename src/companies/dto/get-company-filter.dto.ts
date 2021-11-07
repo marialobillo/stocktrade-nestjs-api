45b0b086a555edbf137821a5355a8e01fde023c1
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-11-07 11:52:53 +0100

    validation for search company

--- src/companies/dto/get-company-filter.dto.ts
+++ src/companies/dto/get-company-filter.dto.ts
@@ -1,4 +1,11 @@
+import { IsOptional, IsString } from "class-validator";
+
 export class GetCompanyFilterDto {
+  @IsOptional()
+  @IsString()
   name?: string;
+
+  @IsOptional()
+  @IsString()
   search?: string;
 }
