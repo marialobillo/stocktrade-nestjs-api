ddec8eaf9037692f99ee97090a1a59e6a36e0110
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-11-07 18:41:22 +0100

    companies repository

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -1,8 +1,13 @@
+import { CompaniesRepository } from './companies.repository';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompaniesController } from './companies.controller';
 import { CompaniesService } from './companies.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([CompaniesRepository]),
+  ],
   controllers: [CompaniesController],
   providers: [CompaniesService],
 })
