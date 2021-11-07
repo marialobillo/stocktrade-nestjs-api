d1c14ff02dd4ba89a321b98ba440873060deccfc
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-11-07 13:33:59 +0100

    adding typeorm for connect db with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { CompaniesModule } from './companies/companies.module';
 import { HoldingsModule } from './holdings/holdings.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CompaniesModule, HoldingsModule],
+  imports: [
+    CompaniesModule, 
+    HoldingsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'stocktrade-db',
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
 })
 export class AppModule {}
