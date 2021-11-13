10b43f6173d44fd884e1699fe217b1142ac92b0a
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-11-13 19:39:53 +0100

    auth signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,18 @@
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UsersRepository)
-    private UsersRepository: UsersRepository
+    private usersRepository: UsersRepository
   ) {}
 
 
-  async signUp()
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
 }
