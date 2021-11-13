ae677bdb29b60999fdaf45b5371cdf5a29d175f2
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-11-13 18:38:50 +0100

    service for auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
+import { UsersRepository } from './users.repository';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private UsersRepository: UsersRepository
+  ) {}
+  
+}
