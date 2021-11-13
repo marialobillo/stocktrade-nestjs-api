4e37e6b70adf5e9c525f7fa3b1edeee7049202b9
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-11-13 17:30:47 +0100

    update dto for holdings

--- src/holdings/dto/update-holding.dto.ts
+++ src/holdings/dto/update-holding.dto.ts
@@ -1,5 +1,5 @@
 import { IsNotEmpty } from 'class-validator';
-export class UpdateCompanyNameDto{
+export class UpdateHoldingDto{
   @IsNotEmpty()
   priceSell: number;
 
--- src/holdings/holdings.controller.ts
+++ src/holdings/holdings.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Query, Patch } from '@nestjs/common';
 import { CreateHoldingDto } from './dto/create-holding.dto';
 import { GetHoldingFilterDto } from './dto/get-holding-filter.dto';
+import { UpdateHoldingDto } from './dto/update-holding.dto';
 import { Holding } from './holding.model';
 import { HoldingsService } from './holdings.service';
 
@@ -21,12 +22,21 @@ export class HoldingsController {
     }
 
     @Get('/:id')
-    getHoldingById(@Param('id') id: string): Holding {
+    getHoldingById(@Param('id') id: string): Promise<Holding> {
       return this.holdingsService.getHoldingById(id);
     }
 
     @Delete('/:id')
-    deleteHolding(@Param('id') id: string): void {
+    deleteHolding(@Param('id') id: string): Promise<void> {
       return this.holdingsService.deleteHolding(id);
+    }
+
+    @Patch('/:id')
+    updateHolding(
+      @Param('id') id: string,
+      @Body() updateHoldingDto: UpdateHoldingDto,
+    ): Promise<Holding> {
+      const { priceSell, dateSell } = updateHoldingDto;
+      return this.holdingsService.updateHolding(id);
     }
 }
